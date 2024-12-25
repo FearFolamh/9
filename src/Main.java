1dc9e17868d6f30b8f67ed199a9d75f5d8a56acf
I-Unknown-I <62710051+I-Unknown-I@users.noreply.github.com>
2024-12-25 18:59:10 +0300

    0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Main extends JPanel {
+    private final String[] strings = {"Привет", "Мир", "Java", "Swing", "AWT"};
+    private final int[] xPositions;
+    private final int[] yPositions;
+    private final int[] xVelocities;
+    private final int[] yVelocities;
+
+    public Main() {
+        Random random = new Random();
+        int stringCount = strings.length;
+        xPositions = new int[stringCount];
+        yPositions = new int[stringCount];
+        xVelocities = new int[stringCount];
+        yVelocities = new int[stringCount];
+
+        for (int i = 0; i < stringCount; i++) {
+            xPositions[i] = random.nextInt(300);
+            yPositions[i] = random.nextInt(300);
+            xVelocities[i] = random.nextInt(5) + 1;
+            yVelocities[i] = random.nextInt(5) + 1;
+        }
+
+        Timer timer = new Timer(30, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < stringCount; i++) {
+                    xPositions[i] += xVelocities[i];
+                    yPositions[i] += yVelocities[i];
+
+                    if (xPositions[i] < 0 ||  xPositions[i] > getWidth() - 50) {
+                        xVelocities[i] *= -1;
+                    }
+                    if (yPositions[i] < 0  || yPositions[i] > getHeight() - 10) {
+                        yVelocities[i] *= -1;
+                    }
+                }
+                repaint();
+            }
+        });
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        for (int i = 0; i < strings.length; i++) {
+            g.drawString(strings[i], xPositions[i], yPositions[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Движение строк");
+        Main applet = new Main();
+        frame.add(applet);
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
